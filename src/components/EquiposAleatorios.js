3bd6ddd06f33b5ecd743c9255d0b0cf7b9d2f64b
reparaz01 <jreparazm@gmail.com>
2023-11-17 12:55:18 +0100

    1

---
+++ src/components/EquiposAleatorios.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import Global from '../Global';
+import axios from 'axios';
+import './EquiposAleatorios.css'; // Importar el archivo de estilos CSS
+
+class EquiposAleatorios extends Component {
+  state = {
+    alumnos: [],
+    teams: Array.from({ length: 8 }, () => []), // Inicializar teams como un array vacío de 8 elementos
+  };
+
+  loadEstudiantes = () => {
+    const request = "api/alumnos/filtrarcurso/2023";
+    const url = Global.urlApi + request;
+
+    axios.get(url)
+      .then(response => {
+        const alumnos = response.data;
+
+        this.setState({
+          alumnos: alumnos,
+          status: true,
+        });
+      })
+      .catch(error => {
+        console.error('Error al obtener la lista de alumnos:', error);
+      });
+  };
+
+  componentDidMount() {
+    this.loadEstudiantes();
+  }
+
+  generarEquipos = () => {
+    const alumnosCopy = [...this.state.alumnos];
+    const alumnosDesordenados = alumnosCopy.sort(() => Math.random() - 0.5);
+    const alumnosPorEquipo = Math.ceil(alumnosDesordenados.length / 8);
+    const equipos = [];
+
+    for (let i = 0; i < 8; i++) {
+      const equipo = alumnosDesordenados.splice(0, alumnosPorEquipo);
+      equipos.push(equipo);
+    }
+
+    this.setState({ teams: equipos });
+  };
+
+  render() {
+    return (
+      <div className="main-container">
+        <div className="equipos-aleatorios-container">
+          <div className="tabla-alumnos-container">
+            <ul className="alumnos-list">
+              {this.state.alumnos.map((alumno, index) => (
+                <li key={index}>{alumno.nombre} {alumno.apellidos}</li>
+              ))}
+            </ul>
+            <button onClick={this.generarEquipos}>Generar Equipos</button>
+          </div>
+          <div className="grilla-equipos-container">
+            <h2>Equipos</h2>
+            <div className="equipos-grid">
+              {this.state.teams.map((equipo, index) => (
+                <div key={index} className="equipo-contenedor">
+                  <h3>Equipo {index + 1}</h3>
+                  <table className="bordered-table equipos-table">
+                    <thead>
+                      <tr>
+                        <th>Nombre</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {equipo.map((alumno, alumnoIndex) => (
+                        <tr key={alumnoIndex}>
+                          <td>{alumno.nombre} {alumno.apellidos}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default EquiposAleatorios;
